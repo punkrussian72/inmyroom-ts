32affa400d0b82e0a9f37b0d147104dbd548b7c5
Ivan Pankrashin <pankrashin.ivan@gmail.com>
2020-06-04 18:00:43 +0200

    main elements positioning started work on tumba-item layout

--- src/components/TumbaItem.tsx
+++ src/components/TumbaItem.tsx
@@ -8,23 +8,25 @@ const Tumba = () => {
   const sales = ['Огого', 'Laska Family', 'Garda', 'DG Home'];
   return (
     <div className="tumba-item">
-      <img src={tumbaImage} alt="Tumba" className={'photo'} width='148'/>
-      <span className="name">Тумба прикроватная Rubus с двумя ящиками</span>
-      <div className="rating">
+      <div className="photo">
+        <img src={tumbaImage} alt="Tumba" className={'photo'}/>
+      </div>
+      <div className="name">Тумба прикроватная Rubus с двумя ящиками</div>
+      <span className="rating">
         <img src={starImage} alt="star" width='15'/>
         <img src={starImage} alt="star" width='15'/>
         <img src={starImage} alt="star" width='15'/>
         <img src={starImage} alt="star" width='15'/>
         <img src={starImage} alt="star" width='15'/>
         <span className="rate">4.8</span>
-      </div>
+      </span>
       <span className="price">56 720 P</span>
-      <span>Черный</span>
-      <span>Ткань</span>
-      <span>ш. 349 х в. 234 х г. 323</span>
-      <span>Французская раскладушка</span>
+      <span className="info">Черный</span>
+      <span className="info">Ткань</span>
+      <span className="info">ш. 349 х в. 234 х г. 323</span>
+      <span className="info">Французская раскладушка</span>
       {/* eslint-disable-next-line*/}
-      <a href="#">{sales[Math.floor(Math.random() * sales.length)]}</a>
+      <span className="info"><a href="#">{sales[Math.floor(Math.random() * sales.length)]}</a></span>
       <div className="buttons-panel">
         <button className="heart"><img src={heartImage} alt="heart"/></button>
         <button className="buy"><img src={cartImage} alt="" width='20'/>Купить</button>

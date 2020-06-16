37b80c82a4b87769b880c2221bd2d6067bbb32a4
Ivan Pankrashin <pankrashin.ivan@gmail.com>
2020-06-16 14:27:45 +0200

    Updates

---
+++ precache-manifest.d30b659443449a996e522aa57f4194e4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d10fbd21ba86493e6f1e72f2a391cb82",
+    "url": "/inmyroom-ts/index.html"
+  },
+  {
+    "revision": "79c9a0c43db90a6871ed",
+    "url": "/inmyroom-ts/static/css/main.c8e4255b.chunk.css"
+  },
+  {
+    "revision": "96fbb9616da1564038cb",
+    "url": "/inmyroom-ts/static/js/2.c79057cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/inmyroom-ts/static/js/2.c79057cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79c9a0c43db90a6871ed",
+    "url": "/inmyroom-ts/static/js/main.3806992d.chunk.js"
+  },
+  {
+    "revision": "98f40b59d31d099f9e23",
+    "url": "/inmyroom-ts/static/js/runtime-main.8f65ebf5.js"
+  },
+  {
+    "revision": "8b0126c677112ff8ac1c581569efaae2",
+    "url": "/inmyroom-ts/static/media/heart.8b0126c6.svg"
+  },
+  {
+    "revision": "802c2910a4262429212fda2c55a13ec7",
+    "url": "/inmyroom-ts/static/media/tumba.802c2910.png"
+  }
+]);
